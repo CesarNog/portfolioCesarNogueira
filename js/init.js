07f2ee4842b1d459a546e89debe331d3ab3c26cc
Cesar Augusto Nogueira <cesarnogueira1210@gmail.com>
2014-10-29 22:22:38 -0200

    Links Social and texts - First commit
    Social links top
    Contact me text
    Skills

--- js/init.js
+++ js/init.js
@@ -27,7 +27,7 @@
 
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
+	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
@@ -175,6 +175,39 @@
       });
       return false;
    });
+   
+   
+
+/*----------------------------------------------------*/
+/*	Modernizer popups effects
+------------------------------------------------------*/   
+$('#inline-popups').magnificPopup({
+  delegate: 'a',
+  removalDelay: 500, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeOpen: function() {	
+       this.st.mainClass = this.st.el.attr('data-effect');
+    }
+  },
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
+
+// Hinge effect popup
+$('a.hinge').magnificPopup({
+  mainClass: 'mfp-with-fade',
+  removalDelay: 1000, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeClose: function() {
+        this.content.addClass('hinge');
+    }, 
+    close: function() {
+        this.content.removeClass('hinge'); 
+    }
+  },
+  midClick: true
+});
+
+/*------------------------------------------------------*/  
 
 
 });
